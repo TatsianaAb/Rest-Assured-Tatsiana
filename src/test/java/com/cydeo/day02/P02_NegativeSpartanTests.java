d3229f4f6b20815d46fd2f2a7ac5729a46c251d8
mehmetCt <mehmet@cybertekschool.com>
2022-12-05 11:11:59 -0500

    Day03

--- src/test/java/com/cydeo/day02/P02_NegativeSpartanTests.java
+++ src/test/java/com/cydeo/day02/P02_NegativeSpartanTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class P02_NegativeSpartanTests {
---
+++ src/test/java/com/cydeo/day03/P01_SpartanWithPathParam.java
@@ -0,0 +1,24 @@
+package com.cydeo.day03;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class P01_SpartanWithPathParam extends SpartanTestBase {
+
+    /*   Given accept type is Json
+        And Id parameter value is 24
+        When user sends GET request to /api/spartans/{id}
+        Then response status code should be 200
+        And response content-type: application/json
+        And "Blythe" should be in response payload(body)
+     */
+    @DisplayName("GET Spartan /api/spartans/{id} path param with 24  ")
+    @Test
+    public void test1() {
+
+
+
+
+    }
+}
