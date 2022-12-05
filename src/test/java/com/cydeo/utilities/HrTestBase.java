a647620d0234f26e6297bcab40cd2b5f278e80f1
mehmetCt <mehmet@cybertekschool.com>
2022-12-05 12:46:14 -0500

    Day03

---
+++ src/test/java/com/cydeo/utilities/HrTestBase.java
@@ -0,0 +1,17 @@
+package com.cydeo.utilities;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class HrTestBase {
+
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI="http://3.80.111.193:1000/ords/hr";
+
+        //RestAssured.baseURI= "YOURIP:1000/ords/hr";
+
+    }
+
+}
