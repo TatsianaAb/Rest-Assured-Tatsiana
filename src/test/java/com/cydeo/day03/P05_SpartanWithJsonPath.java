894f31e667adccbedd2905dfd447779409b4fe5b
mehmetCt <mehmet@cybertekschool.com>
2022-12-05 16:30:03 -0500

    Day03

--- src/test/java/com/cydeo/day03/P05_SpartanWithJsonPath.java
+++ src/test/java/com/cydeo/day03/P05_SpartanWithJsonPath.java
@@ -2,6 +2,7 @@ package com.cydeo.day03;
 
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,7 +37,23 @@ public class P05_SpartanWithJsonPath extends SpartanTestBase {
         //Then status code is 200
         assertEquals(200, response.statusCode());
 
+        System.out.println(response.path("id").toString());
 
+        // we saved response as JSONPATH OBJECT
+        JsonPath jsonPath = response.jsonPath();
+
+        // is it possible to get statusCode / ContentType / Headers with JsonPath
+        // if you wanna do assertion for then still we need to use response Object
+
+        System.out.println("jsonPath.getInt(\"id\") = " + jsonPath.getInt("id"));
+
+        int id = jsonPath.getInt("id");
+        String name = jsonPath.getString("name");
+        String gender = jsonPath.getString("gender");
+        long phone = jsonPath.getLong("phone");
+
+        //Assertion
+        assertEquals(10,id);
 
 
 
