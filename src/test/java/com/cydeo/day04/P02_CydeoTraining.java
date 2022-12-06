c8a315c675513611fa29509fddd9768c86ae12a8
mehmetCt <mehmet@cybertekschool.com>
2022-12-06 12:17:23 -0500

    Day04

--- src/test/java/com/cydeo/day04/P02_CydeoTraining.java
+++ src/test/java/com/cydeo/day04/P02_CydeoTraining.java
@@ -3,7 +3,30 @@ package com.cydeo.day04;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class P02_CydeoTraining {
+
+      /*
+    Given accept type is application/json
+    And path param is 2
+    When user send request /student/{id}
+    Then status code should be 200
+    And content type is application/json;charset=UTF-8
+    And Date header is exist
+    And Server header is envoy
+    And verify following
+                firstName Mark
+                batch 13
+                major math
+                emailAddress mark@email.com
+                companyName Cydeo
+                street 777 5th Ave
+                zipCode 33222
+
+
+     */
 
     @DisplayName("GET /student/{id} 2 ")
     @Test
