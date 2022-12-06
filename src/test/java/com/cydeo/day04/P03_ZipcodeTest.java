01bec8a163c9abe5060c37eb03bb3435e9338ce7
mehmetCt <mehmet@cybertekschool.com>
2022-12-06 13:02:23 -0500

    Day04

---
+++ src/test/java/com/cydeo/day04/P03_ZipcodeTest.java
@@ -0,0 +1,23 @@
+package com.cydeo.day04;
+
+public class P03_ZipcodeTest {
+
+    /**
+     * Create TestBase for zipcode
+     *
+     *
+     */
+
+    /**
+     Given accept type is json
+     and country path param value is "us"
+     and postal code path param value is 22102
+     When I send get request to http://api.zippopotam.us/{country}/{postal-code}
+     Then status code is 200
+     Then "post code" is "22102"
+     And  "country" is "United States"
+     And "place name" is "Mc Lean"
+     And  "state" is "Virginia"
+     */
+
+}
