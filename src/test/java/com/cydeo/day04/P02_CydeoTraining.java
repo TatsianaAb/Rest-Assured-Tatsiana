cba2d2055baae40beb9eb3b3b298a9d3c185adcc
mehmetCt <mehmet@cybertekschool.com>
2022-12-06 11:53:45 -0500

    Day04

---
+++ src/test/java/com/cydeo/day04/P02_CydeoTraining.java
@@ -0,0 +1,15 @@
+package com.cydeo.day04;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class P02_CydeoTraining {
+
+    @DisplayName("GET /student/{id} 2 ")
+    @Test
+    public void test1() {
+
+
+
+    }
+}
