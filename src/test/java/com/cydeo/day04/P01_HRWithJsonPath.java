3d740a904d8a7903a3b182851a26397b23c217cd
mehmetCt <mehmet@cybertekschool.com>
2022-12-06 10:44:08 -0500

    Day04

---
+++ src/test/java/com/cydeo/day04/P01_HRWithJsonPath.java
@@ -0,0 +1,18 @@
+package com.cydeo.day04;
+
+import com.cydeo.utilities.HrTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class P01_HRWithJsonPath extends HrTestBase {
+
+    @DisplayName("Get All employees with Query Param limit = 200 --> JSONPATH ")
+    @Test
+    public void test1() {
+
+
+    }
+}
