ca1825d112b394a7bf95b248c47e0cc06ff3ec39
mehmetCt <mehmet@cybertekschool.com>
2022-12-06 10:49:36 -0500

    Day04

--- src/test/java/com/cydeo/day04/P01_HRWithJsonPath.java
+++ src/test/java/com/cydeo/day04/P01_HRWithJsonPath.java
@@ -1,6 +1,8 @@
 package com.cydeo.day04;
 
 import com.cydeo.utilities.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +11,26 @@ import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class P01_HRWithJsonPath extends HrTestBase {
 
+
+    /*
+    Given accept type is application/json
+    And query param limit is 200
+    When user send request /employees
+    Then user should see ............
+
+     */
+
     @DisplayName("Get All /employees?limit=200 --> JSONPATH ")
     @Test
     public void test1() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and() // it increases readablitity
+                .queryParam("limit", 200).
+                when().get("/employees");
+
+
+        response.prettyPrint();
 
 
     }
