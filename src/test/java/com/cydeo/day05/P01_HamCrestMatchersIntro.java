d393892f4fd18c56e8c761a49c2e9f61cf034b1f
mehmetCt <mehmet@cybertekschool.com>
2022-12-08 11:20:59 -0500

    Day05

--- src/test/java/com/cydeo/day05/P01_HamCrestMatchersIntro.java
+++ src/test/java/com/cydeo/day05/P01_HamCrestMatchersIntro.java
@@ -3,6 +3,9 @@ package com.cydeo.day05;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -82,6 +85,31 @@ public class P01_HamCrestMatchersIntro {
 
     }
 
+    @Test
+    public void testCollections() {
+
+        List<Integer> numberLst = Arrays.asList( 3, 5, 1, 77, 44, 76 ) ; // 6 elements here
+
+        // how to check size of elements
+        assertThat(numberLst,hasSize(6));
+
+        // how to check 44 is into collection
+        assertThat(numberLst,hasItem(44));
+        // assertThat(numberLst,hasItem(2));  to make it fail
+
+        // how to check 44 and 76 is into collection
+        assertThat(numberLst,hasItems(44,76));
+        assertThat(numberLst,hasItems(44,76,1));
+        // assertThat(numberLst,hasItems(44,76,1,2)); to make it fail
+
+        assertThat(numberLst,hasItems(greaterThan(70)));
 
 
+        //everyItem --> check each element into array about realted condition
+        assertThat(numberLst,everyItem(greaterThanOrEqualTo(1)));
+
+        assertThat(numberLst,containsInRelativeOrder(3, 5, 1, 77, 44, 76));
+
+
+    }
 }
