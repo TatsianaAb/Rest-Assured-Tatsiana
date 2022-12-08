d0e5853dbfb51f6605f86effee89869a2d80d2bb
mehmetCt <mehmet@cybertekschool.com>
2022-12-08 16:14:50 -0500

    Day05

--- src/test/java/com/cydeo/day05/P04_DeserilizationToCollections.java
+++ src/test/java/com/cydeo/day05/P04_DeserilizationToCollections.java
@@ -4,8 +4,10 @@ import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -58,6 +60,65 @@ public class P04_DeserilizationToCollections extends SpartanTestBase {
                 String nameJson = (String) jsonPathMap.get("name");
 
                 System.out.println("idJson = " + idJson);
+
+
+    }
+
+    @DisplayName("GET All Spartans with Java Collections")
+    @Test
+    public  void test2() {
+
+        Response response = given().accept(ContentType.JSON).
+                when().get("/api/spartans").
+                then()
+                .statusCode(200)
+                .contentType("application/json")
+                .extract().response();
+
+        //Approach one ---> with Response
+
+            List<Map<String,Object>> spartanList = response.as(List.class);
+
+
+            for (Map<String, Object> eachMap : spartanList) {
+                System.out.println("eachMap = " + eachMap);
+            }
+
+            // How to find first spartan info
+            System.out.println("spartanList.get(0) = " + spartanList.get(0));
+            // How to find first spartan name
+            System.out.println("spartanList.get(0).get(\"name\") = " + spartanList.get(0).get("name"));
+            // How to find first spartan id
+            System.out.println("spartanList.get(0).get(\"id\") = " + spartanList.get(0).get("id"));
+
+
+            // how to store first spartan info as a map with response.as() method
+            // if yo uwanna convert only specific part of response with response.as() it does not have any parameters to get as path of
+            // json object.That is why we can use in here response.path() method to convert this as a Object.Since we know the type of it
+            // we can use and store as Map
+
+            Map<String, Object> firstSpartanMap = response.path("[0]");
+            System.out.println("firstSpartanMap = " + firstSpartanMap);
+
+
+
+        // Approach second ---> with JSONPATH
+        JsonPath jsonPath = response.jsonPath();
+        List<Map<String, Object>> allSpartanList = jsonPath.getList("");
+
+        for (Map<String, Object> eachSpartan : allSpartanList) {
+            System.out.println("eachSpartan = " + eachSpartan);
+        }
+
+        // How to find first spartan info
+        System.out.println("allSpartanList.get(0) = " + allSpartanList.get(0));
+        // How to find first spartan name
+        System.out.println("allSpartanList.get(0).get(\"name\") = " + allSpartanList.get(0).get("name"));
+        // How to find first spartan id
+        System.out.println("allSpartanList.get(0).get(\"id\") = " + allSpartanList.get(0).get("id"));
+
+
+
 
 
     }
