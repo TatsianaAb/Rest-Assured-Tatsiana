f6cda92359b8baa80ae6afbcc2158cb78b7b9d2a
mehmetCt <mehmet@cybertekschool.com>
2022-12-09 15:57:44 -0500

    Day06

---
+++ src/test/java/com/cydeo/day06/P03_HRDeserializationPOJO.java
@@ -0,0 +1,20 @@
+package com.cydeo.day06;
+
+import com.cydeo.utilities.HrTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class P03_HRDeserializationPOJO extends HrTestBase {
+
+
+    @DisplayName("GET regions to deserilization to POJO - LOMBOK - JSON PROPERTY")
+    @Test
+    public void test1() {
+
+
+
+
+
+
+    }
+}
